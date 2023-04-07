3018a95e38631be65a6aaf259423f6916a245bcf
Asma Ali <128274795+AsmaAl12@users.noreply.github.com>
2023-04-07 18:38:19 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,117 @@
+//Q1.
+//let num_1 = +prompt("enter no 1:");
+//let num_2 = +prompt("enyer no 2:");
+
+//if(num_1 > num_2){
+//   console.log(num_1 + " is greater");
+//}
+//else if(num_2 > num_1){
+//   console.log(num_2 + " is greater");
+//}
+//else{
+//   console.log("both nos are equal");
+//}
+
+
+
+//Q2.
+// let num = +prompt("enter no:");
+// if (num > 0){
+//     console.log("+ sign");
+// }
+// else if (num < 0){
+//     console.log("- sign");
+// }
+// else if (num === 0){
+//     console.log("entered no is zero");
+// }
+
+
+
+//Q3.
+// let num_1 = +prompt("enter no 1:");
+// let num_2 = +prompt("enter no 2:");
+// let num_3 = +prompt("enter no 3:");
+// let num_4 = +prompt("enter no 4:");
+// let num_5 = +prompt("enter no 5:");
+
+// if(num_1 > num_2 && num_1 > num_3 && num_1 > num_4 && num_1 > num_5){
+//      console.log(num_1 + " is greater");
+// }
+// else if(num_2 > num_1 && num_2 > num_3 && num_2 > num_4 && num_2 > num_5){
+//     console.log(num_2 + " is greater");
+// }
+// else if(num_3 > num_1 && num_3 > num_2 && num_3 > num_4 && num_3 > num_5){
+//     console.log(num_3 + " is greater");
+// }
+// else if(num_4 > num_1 && num_4 > num_2 && num_4 > num_3 && num_4 > num_5){
+//     console.log(num_4 + " is greater");
+// }
+// else if(num_5 > num_1 && num_5 > num_2 && num_5 > num_3 && num_5 > num_4){
+//     console.log(num_5 + " is greater");
+// }
+// else{
+//     console.log("no number is greater you have entered equal numbers")
+// }
+
+
+
+//Q4.
+// for (i=0; i<=15; i++){
+//     if (i % 2 == 0){
+//         console.log(i + " is even");
+//     }
+//     else{
+//         console.log(i + " is odd");
+//     }
+// }
+
+
+
+//Q5.
+// let num = +prompt("Type your percentage");
+// let grade;
+// if (num<=100 && num>=90) {
+// grade="A"
+// }
+// else if (num<=90) {
+//    grade="B" 
+// }
+// else if (num<=80) {
+//    grade="C" 
+// }
+// else if (num<=70) {
+//    grade="D" 
+// }
+// else if (num<=60) {
+//    grade="F" 
+// }
+// console.log(grade);
+
+
+
+//Q6.
+// for (i=1; i<=100; i++){
+//     if (i % 3 ==0){
+//         console.log(i + " Fizz");
+//     }
+//     else if (i % 5 ==0){
+//         console.log(i + " Buzz");
+//     }
+//     else if (i % 3 == 0 && i % 5 ==0){
+//         console.log(i + " FizzBuzz")
+//     }
+// }
+
+
+
+//Q7.
+//nested for loop
+// for(i = 1; 1<=5; i++){
+//     for(j = 1; j<=i; j++){
+//         document.write("*")
+//     }
+//     document.write("<br>")
+// }
+
+//=======================ASSIGNMENT COMPLETED====================
